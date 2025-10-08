a26c5c692397f2e1838f2cc80609eabe3772ba5d
mbsantiago <santiago.mbal@gmail.com>
2025-10-08 15:03:57 +0100

    Download speciesnet in the setup

--- CameraTrapsAI/ct_notebook_setup.sh
+++ CameraTrapsAI/ct_notebook_setup.sh
@@ -25,15 +25,32 @@ source .mdvenv/bin/activate
 
 # Install megadetector
 if ! uv pip show megadetector; then
+    echo "Installing megadetector"
     uv pip install megadetector
 fi
 
 # Install speciesnet
 if ! uv pip show speciesnet; then
+    echo "Installing speciesnet"
     uv pip install speciesnet
 fi
 
 # Unzip data
 if [ ! -d data ]; then
+    echo "Unzipping data"
     unzip drive/MyDrive/AI-Intervene-Camera-Traps/data.zip -d .
+fi
+
+# Download models
+if [ ! -d models ]; then
+    mkdir models
+fi
+
+# Download speciesnet model
+if [ ! -d models/speciesnet ]; then
+    echo "Downloading speciesnet model"
+    mkdir models/speciesnet
+    wget -q -o model.tar.gz https://www.kaggle.com/api/v1/models/google/speciesnet/pyTorch/v4.0.1a/1/download
+    tar -xvf model.tar.gz --directory models/speciesnet
+    rm model.tar.gz
 fi
