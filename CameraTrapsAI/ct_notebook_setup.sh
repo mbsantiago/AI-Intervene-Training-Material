052d99c9dd61e3110ef745d8227a1415c6de929b
mbsantiago <santiago.mbal@gmail.com>
2025-10-08 11:59:52 +0100

    Update links of setup and util scripts to use the repo versions

--- CameraTrapsAI/ct_notebook_setup.sh
+++ CameraTrapsAI/ct_notebook_setup.sh
@@ -6,13 +6,13 @@ export UV_SYSTEM_PYTHON=false
 # Download colab utils
 if [ ! -f colab_utils.py ]; then
     echo "Downloading colab_utils.py"
-    wget -q https://gist.github.com/mbsantiago/f51815e0255f3b6ae6047d6115a119da/raw/e37d2722ae956bd41e59bf13c888467b0b22f740/colab_utils.py 
+    wget -q https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/CameraTrapsAI/colab_utils.py
 fi
 
 # Download ct notebook utils
 if [ ! -f ct_notebook_utils.py ]; then
     echo "Downloading ct_notebook_utils.py"
-    wget -q https://gist.github.com/mbsantiago/872f49a0656aea0b43377619bdbacb4b/raw/59dbc82ab6bbbe0376badb751d5668886f0b33ef/ct_notebook_utils.py
+    wget -q https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/CameraTrapsAI/ct_notebook_utils.py
 fi
 
 # Create virtual environment
