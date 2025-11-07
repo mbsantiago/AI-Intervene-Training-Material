33aba23312ac3feae8e8e862a30a02dd39ad7025
mbsantiago <santiago.mbal@gmail.com>
2025-11-07 20:24:17 +0000

    Fix zip name and download util modules

--- BioacousticsAI/setup.sh
+++ BioacousticsAI/setup.sh
@@ -8,4 +8,9 @@ uv pip install "batdetect2==1.0.8"
 gdown 1UWPoP6J9bh9l7Q-cRYsd2I6rrQGnjMUJ
 
 # Unzip data
-unzip data.zip
+unzip acoustic_data.zip
+
+# Download utility modules
+wget https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/BioacousticsAI/evaluation_utils.py
+wget https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/BioacousticsAI/plotting_utils.py
+wget https://github.com/mbsantiago/AI-Intervene-Training-Material/raw/refs/heads/main/BioacousticsAI/audio_utils.py
